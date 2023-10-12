07b8185b4d87e4bcf6ba3de41812f8ff9fa61ee5
HASERE <1376203676@qq.com>
2023-10-12 15:21:49 +0800

    社区DAO更新 点赞收藏评论的功能

--- src/main/java/com/gduf/dao/CommunityDAO.java
+++ src/main/java/com/gduf/dao/CommunityDAO.java
@@ -12,18 +12,40 @@ import java.util.List;
 @Mapper
 public interface CommunityDAO {
 
-    @Insert("insert into tb_community (title,article,create_time) values (#{title},#{article},#{createTime})")
+    @Insert("insert into tb_post (title,article,create_time) values (#{title},#{article},#{createTime})")
     public int insertPost(Post post);
 
-    @Select("select * from tb_community")
+    @Select("select * from tb_post")
     public List<Post> showAllPost();
 
-    @Select("select * from tb_community where writer_id = #{writerId}")
+    @Select("select * from tb_post where writer_id = #{writerId}")
     public List<Post> showPostByWriter(Integer writerId);
 
-    @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
-    public int update(Book book);
+//    当点赞的时候 更新社区表中点赞数量
+    @Update("update tb_post set likes = (likes + 1) where post_id = #{postId}")
+    public void updateLikesInCommunity(Integer postId);
 
-    @Update("update tb_community set likes = (likes + 1)")
-    public void insetLikes();
+//    更新收藏
+    @Update("update tb_post set stars = (stars + 1) where post_id = #{postId}")
+    public void updateStarsInCommunity(Integer postId);
+
+//    更新评论
+    @Update("update tb_post set comments = (comments + 1) where post_id = #{postId}")
+    public void updateCommentsInCommunity(Integer postId);
+
+    @Update("update tb_comments set likes = (likes + 1) where comment_id = #{commentId} ")
+    public void updateLikesForCommentsInCommunity(Integer commentId);
+
+//    关系表增加
+    @Insert("insert into user_like_posts (user_id,post_id) values (#{userId},#{postId}) ")
+    public void insetLikesInUser(Integer postId, Integer userId);
+
+    @Insert("insert into user_star_posts (user_id,post_id) values (#{userId},#{postId})")
+    public void insertStarsInUser(Integer postId, Integer userId);
+
+    @Insert("insert into tb_comment (comment_msg,post_id) values (#{commentMsg},#{postId})")
+    public void insertComment(String commentMsg,Integer postId);
+
+    @Insert("insert into user_like_comments (user_id,comment_id) values (#{userId},#{commentId})")
+    public void insertLikeCommentInUser(Integer userId,Integer commentId);
 }
